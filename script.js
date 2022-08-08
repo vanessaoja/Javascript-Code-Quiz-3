f52980125ff0e229a593d3d3bb3ce03dfbe45e01
vanessaoja <ojavanessa@gmail.com>
2022-08-08 17:02:39 -0500

    added start button

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ var buttonDEL = document.getElementById("answerD");
 var answersEl = document.getElementById("answers");
 var startBtn = document.getElementById("startBtn");
 var timerEl = document.getElementById("timer")
+
+var initialsEl = document.getElementById("score");
+var initialsBtnEl = document.getElementById("initBtn");
+var qADiv = document.getElementById("q-a-div");
+
+document.getElementById("startBtn") .addEventListener("click", function () {
+    startQuiz();
+})
