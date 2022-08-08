487fac0b6ffcbc34a7a7834f6a653c28b12c87b8
vanessaoja <ojavanessa@gmail.com>
2022-08-08 16:53:27 -0500

    created start and timer btns

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ var buttonAEL = document.getElementById("answerA");
 var buttonBEL = document.getElementById("answerB");
 var buttonCEL = document.getElementById("answerC");
 var buttonDEL = document.getElementById("answerD");
+var answersEl = document.getElementById("answers");
+var startBtn = document.getElementById("startBtn");
+var timerEl = document.getElementById("timer")
